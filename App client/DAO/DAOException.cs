9efd00f7a29462b717ee9042865422dd9d8d4c5f
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-29 14:15:57 +0100

    better exceptions

--- App client/DAO/DAOException.cs
+++ App client/DAO/DAOException.cs
@@ -8,8 +8,22 @@ namespace DAO
 {
     public class DAOException : Exception
     {
-        public DAOException(string? message = null) : base(message)
+        public DAOException(string? message) : base(message) => Code = ErrorCode.UNKNOWN;
+
+        public DAOException() : base() => Code = ErrorCode.UNKNOWN;
+
+        public DAOException(string? message, ErrorCode code) : base(message) => Code = code;
+
+        public DAOException(ErrorCode code) : base() => Code = code;
+
+        public enum ErrorCode
         {
+            UNKNOWN,
+            MISSING_ENTRY,
+            ENTRY_LINKED,
+            EXISTING_ENTRY
         }
+
+        public ErrorCode Code { get; init; }
     }
 }
