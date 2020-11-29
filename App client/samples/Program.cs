45a475fb75343c09a5e005cc8170bfea53d9413c
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-29 11:07:11 +0100

    added C# projects

---
+++ App client/samples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace samples
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+        }
+
+        #endregion Private Methods
+    }
+}
