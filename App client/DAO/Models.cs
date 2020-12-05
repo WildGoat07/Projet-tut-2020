cfeb9a633367fe3fc65cb821a70769cb32682182
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-12-05 17:59:48 +0100

    meilleurs noms de propriétés models

--- App client/DAO/Models.cs
+++ App client/DAO/Models.cs
@@ -25,8 +25,8 @@ namespace DAO
     (
         string Code,
         string LibelleDiplome,
-        int Vdi,
-        string LibelleVdi,
+        int Version,
+        string LibelleVersion,
         int? AnneeDebut = null,
         int? AnneeFin = null
     );
@@ -35,12 +35,12 @@ namespace DAO
         string Code,
         string Libelle,
         char? Nature = 'E',
-        int? HCM = 0,
-        int? HEI = 0,
-        int? HTD = 0,
-        int? HTP = 0,
-        int? HTPL = 0,
-        int? HPRJ = 0,
+        int? HeuresCM = 0,
+        int? HeuresEI = 0,
+        int? HeuresTD = 0,
+        int? HeuresTP = 0,
+        int? HeuresTPL = 0,
+        int? HeuresPRJ = 0,
         int? NombreEpreuves = 1,
         int? CNU = 2700,
         int? NumCategorie = null,
@@ -53,8 +53,8 @@ namespace DAO
         string Nom,
         string Prenom,
         string? Fonction = null,
-        float? HOblig = null,
-        float? HMax = null,
+        float? HeuresObligees = null,
+        float? HeuresMax = null,
         char? CRCT = 'N',
         char? PesPedr = 'N',
         string? Composante = null
@@ -63,42 +63,42 @@ namespace DAO
     (
         string CodeEc,
         string Annee,
-        int? EffPrev = null,
-        int? EffReel = null,
-        int? GpCM = 1,
-        int? GpEI = 0,
-        int? GpTD = 0,
-        int? GpTP = 0,
-        int? GpTPL = 0,
-        int? GpPRJ = 0,
-        int? GpCMSer = 0,
-        int? GpEISer = 0,
-        int? GpTDSer = 0,
-        int? GpTPSer = 0,
-        int? GpTPLSer = 0,
-        int? GpPRJSer = 0
+        int? EffectifPrevu = null,
+        int? EffectifReel = null,
+        int? GroupesCM = 1,
+        int? GroupesEI = 0,
+        int? GroupesTD = 0,
+        int? GroupesTP = 0,
+        int? GroupesTPL = 0,
+        int? GroupesPRJ = 0,
+        int? GroupesCMServis = 0,
+        int? GroupesEIServis = 0,
+        int? GroupesTDServis = 0,
+        int? GroupesTPServis = 0,
+        int? GroupesTPLServis = 0,
+        int? GroupesPRJServis = 0
     );
     public record Etape
     (
         string Code,
-        int Vet,
+        int VersionEtape,
         string Libelle,
         string Composante,
         string? Diplome = null,
-        int? Vdi = null
+        int? VersionDiplome = null
     );
     public record HorsComp
     (
         string Enseignement,
         string Composante,
         string Annee,
-        int? HCM = 0,
-        int? HEI = 0,
-        int? HTD = 0,
-        int? HTP = 0,
-        int? HTPL = 0,
-        int? HPRJ = 0,
-        float? HEqTD = 0f
+        int? HeuresCM = 0,
+        int? HeuresEI = 0,
+        int? HeuresTD = 0,
+        int? HeuresTP = 0,
+        int? HeuresTPL = 0,
+        int? HeuresPRJ = 0,
+        float? HeuresEquivalentTD = 0f
     );
     public record Semestre
     (
@@ -106,20 +106,20 @@ namespace DAO
         string Libelle,
         int? Numero = null,
         string? Etape = null,
-        int? Vet = null
+        int? VersionEtape = null
     );
     public record Service
     (
         string Enseignant,
         string Ec,
         string Annee,
-        int? NbGpCM = 0,
-        int? NbGpEI = 0,
-        int? NBGpTD = 0,
-        int? NbGpTP = 0,
-        int? NbGpTPL = 0,
-        int? NBGpPRJ = 0,
-        float? HEqTD = 0f
+        int? NombreGroupesCM = 0,
+        int? NombreGroupesEI = 0,
+        int? NombreGroupesTD = 0,
+        int? NombreGroupesTP = 0,
+        int? NombreGroupesTPL = 0,
+        int? NombreGroupesPRJ = 0,
+        float? HeuresEquivalentTD = 0f
     );
     public record Ue
     (
