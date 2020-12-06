b1ee2487ff2bcf81099c7a183312bbe86e4fbc0f
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-12-06 17:07:11 +0100

    test program

--- App client/samples/Program.cs
+++ App client/samples/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace samples
 {
     internal class Program
     {
+        private static HttpClient Client;
+
         #region Private Methods
 
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
+            Client = new HttpClient();
+            await TestRequest();
+        }
+
+        private static async Task TestRequest()
+        {
+            var url = new Uri("http://localhost/Projet-tut-2020/API/enseignant.php");
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            request.Content = new StringContent(
+@"{
+   ""filters"":{
+      ""id_ens"":""cg462"",
+      ""prenom"":""Jean""
+   }
+}", Encoding.UTF8, "application/json");
+            var response = await Client.SendAsync(request);
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
         }
 
         #endregion Private Methods
