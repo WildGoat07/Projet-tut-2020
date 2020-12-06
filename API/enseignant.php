98be51d42fd0a7750dad54c00090023521cc14be
Waynocs <64638869+Waynocs@users.noreply.github.com>
2020-12-06 19:14:18 +0100

    can add enseignant

--- API/enseignant.php
+++ API/enseignant.php
@@ -3,20 +3,16 @@ require_once 'app/Database.php';
 
 header('Content-Type: application/json');
 if ($db == true) {
-    if (!empty($_POST["id_ens"])) {
-        $requete = $db->prepare("SELECT * FROM `enseignant` WHERE `ìd_ens` LIKE :ens");
-        $requete->bindParam(':ens', $_POST["id_ens"]);
-        $requete->execute();
-    } else {
-        $requete = $db->prepare("SELECT * FROM `enseignant`");
-        $requete->execute();
-    }
+
+    $requete = $db->prepare("SELECT * FROM `enseignant`");
+    $requete->execute();
+
+
     $resultats = $requete->fetchAll();
 
+
     $enseignant["success"] = true;
-    $enseignant["message"] = "Voici tous les enseignants";
-    $enseignant["results"]["nbenseignants"] = count($resultats);
-    $enseignant["results"]["enseignants"] = $resultats;
+    $enseignant["values"] = $resultats;
 } else {
     echo json_encode($connectionDB);
 }
