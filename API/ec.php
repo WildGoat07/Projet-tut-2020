c79cf64ff612187ece7a7a48c919fe3de86b857d
Waynocs <64638869+Waynocs@users.noreply.github.com>
2020-12-08 16:02:02 +0100

    filters ec + semestre with cond  ok

--- API/ec.php
+++ API/ec.php
@@ -6,7 +6,283 @@ header('Content-Type: application/json');
 $db = new Database();
 
 if ($db) {
-    $requete = $db->query("SELECT * FROM `ec`");
+    $strReq = "SELECT `code_ec`, `libelle_ec`, `nature`, `HCM`, `HEI`, `HTD`, `HTP`, `HTPL`, `HPRJ`, `NbEpr`, `CNU`, `no_cat`, `code_ec_pere`, `code_ue` FROM `ec`";
+    $postObj = json_decode(file_get_contents('php://input'));
+    if (isset($postObj->filters)) {
+        $firstFilter = true;
+        $whereSet = false;
+        if (isset($postObj->filters->code_ec)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->code_ec as $code_ec) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`code_ec` = \"$code_ec\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->libelle_ec)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->libelle_ec as $libelle_ec) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`libelle_ec` = \"$libelle_ec\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->nature)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->nature as $nature) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`nature` = \"$nature\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->HCM)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $minSet = false;
+            if (isset($postObj->filters->HCM->min)) {
+                $strReq .= "`HCM` >= " . $postObj->filters->HCM->min;
+                $minSet = true;
+            }
+            if (isset($postObj->filters->HCM->max)) {
+                if ($minSet)
+                    $strReq .= " AND ";
+                $strReq .= "`HCM` <= " . $postObj->filters->HCM->max;
+            }
+        }
+        if (isset($postObj->filters->HEI)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $minSet = false;
+            if (isset($postObj->filters->HEI->min)) {
+                $strReq .= "`HEI` >= " . $postObj->filters->HEI->min;
+                $minSet = true;
+            }
+            if (isset($postObj->filters->HEI->max)) {
+                if ($minSet)
+                    $strReq .= " AND ";
+                $strReq .= "`HEI` <= " . $postObj->filters->HEI->max;
+            }
+        }
+        if (isset($postObj->filters->HTD)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $minSet = false;
+            if (isset($postObj->filters->HTD->min)) {
+                $strReq .= "`HTD` >= " . $postObj->filters->HTD->min;
+                $minSet = true;
+            }
+            if (isset($postObj->filters->HTD->max)) {
+                if ($minSet)
+                    $strReq .= " AND ";
+                $strReq .= "`HTD` <= " . $postObj->filters->HTD->max;
+            }
+        }
+        if (isset($postObj->filters->HTP)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $minSet = false;
+            if (isset($postObj->filters->HTP->min)) {
+                $strReq .= "`HTP` >= " . $postObj->filters->HTP->min;
+                $minSet = true;
+            }
+            if (isset($postObj->filters->HTP->max)) {
+                if ($minSet)
+                    $strReq .= " AND ";
+                $strReq .= "`HTP` <= " . $postObj->filters->HTP->max;
+            }
+        }
+        if (isset($postObj->filters->HTPL)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $minSet = false;
+            if (isset($postObj->filters->HTPL->min)) {
+                $strReq .= "`HTPL` >= " . $postObj->filters->HTPL->min;
+                $minSet = true;
+            }
+            if (isset($postObj->filters->HTPL->max)) {
+                if ($minSet)
+                    $strReq .= " AND ";
+                $strReq .= "`HTPL` <= " . $postObj->filters->HTPL->max;
+            }
+        }
+        if (isset($postObj->filters->HPRJ)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $minSet = false;
+            if (isset($postObj->filters->HPRJ->min)) {
+                $strReq .= "`HPRJ` >= " . $postObj->filters->HPRJ->min;
+                $minSet = true;
+            }
+            if (isset($postObj->filters->HPRJ->max)) {
+                if ($minSet)
+                    $strReq .= " AND ";
+                $strReq .= "`HPRJ` <= " . $postObj->filters->HPRJ->max;
+            }
+        }
+        if (isset($postObj->filters->NbEpr)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $minSet = false;
+            if (isset($postObj->filters->NbEpr->min)) {
+                $strReq .= "`NbEpr` >= " . $postObj->filters->NbEpr->min;
+                $minSet = true;
+            }
+            if (isset($postObj->filters->NbEpr->max)) {
+                if ($minSet)
+                    $strReq .= " AND ";
+                $strReq .= "`NbEpr` <= " . $postObj->filters->NbEpr->max;
+            }
+        }
+        if (isset($postObj->filters->CNU)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->CNU as $CNU) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`CNU` = \"$CNU\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->no_cat)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->no_cat as $no_cat) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`no_cat` = \"$no_cat\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->code_ec_pere)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->code_ec_pere as $code_ec_pere) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`code_ec_pere` = \"$code_ec_pere\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->code_ue)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->code_ue as $code_ue) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`code_ue` = \"$code_ue\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+    }
+    if (isset($postObj->order))
+        if (isset($postObj->reverse_order) && $postObj->reverse_order)
+            $strReq .= " ORDER BY DESC `$postObj->order`";
+        else
+            $strReq .= " ORDER BY `$postObj->order`";
+    else if (isset($postObj->reverse_order) && $postObj->reverse_order)
+        $strReq .= " ORDER BY DESC `code_ec`";
+    else
+        $strReq .= " ORDER BY `code_ec`";
+    $strReq .= "LIMIT $postObj->quantity OFFSET $postObj->skip";
+
+    $requete = $db->query($strReq);
 
     $ec = new stdClass();
     $ec->values = [];
--- API/semestre.php
+++ API/semestre.php
@@ -6,30 +6,138 @@ header('Content-Type: application/json');
 $db = new Database();
 
 if ($db) {
-    $requete = $db->query("SELECT * FROM `semestre`");
+    $strReq = "SELECT `code_sem`, `libelle_sem`, `no_sem`, `code_etape`, `vet` FROM `semestre`";
+    $postObj = json_decode(file_get_contents('php://input'));
+    if (isset($postObj->filters)) {
+        $firstFilter = true;
+        $whereSet = false;
+        if (isset($postObj->filters->code_sem)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->code_sem as $code_sem) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`code_sem` = \"$code_sem\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->libelle_sem)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->libelle_sem as $libelle_sem) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`libelle_sem` = \"$libelle_sem\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->no_sem)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->no_sem as $no_sem) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`no_sem` = \"$no_sem\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->code_etape)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->code_etape as $code_etape) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`code_etape` = \"$code_etape\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->vet)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->vet as $vet) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`vet` = \"$vet\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
 
-    $semestre = new stdClass();
-    $semestre->values = [];
+        if (isset($postObj->order))
+            if (isset($postObj->reverse_order) && $postObj->reverse_order)
+                $strReq .= " ORDER BY DESC `$postObj->order`";
+            else
+                $strReq .= " ORDER BY `$postObj->order`";
+        else if (isset($postObj->reverse_order) && $postObj->reverse_order)
+            $strReq .= " ORDER BY DESC `code_sem`";
+        else
+            $strReq .= " ORDER BY `code_sem`";
+        $strReq .= "LIMIT $postObj->quantity OFFSET $postObj->skip";
 
-    foreach ($requete as $req) {
-        $obj = new stdClass();
+        $requete = $db->query($strReq);
 
-        $obj->code_sem = utf8_encode($req['code_sem']);
+        $semestre = new stdClass();
+        $semestre->values = [];
 
-        $obj->libelle_sem = utf8_encode($req['libelle_sem']);
+        foreach ($requete as $req) {
+            $obj = new stdClass();
 
-        $obj->no_sem = utf8_encode($req['no_sem']);
+            $obj->code_sem = utf8_encode($req['code_sem']);
 
-        $obj->code_etape = utf8_encode($req['code_etape']);
+            $obj->libelle_sem = utf8_encode($req['libelle_sem']);
 
-        $obj->vet = utf8_encode($req['vet']);
+            $obj->no_sem = utf8_encode($req['no_sem']);
 
-        $semestre->values[] = $obj;
+            $obj->code_etape = utf8_encode($req['code_etape']);
+
+            $obj->vet = utf8_encode($req['vet']);
+
+            $semestre->values[] = $obj;
+        }
+
+        $semestre->success = true;
+
+        echo json_encode($semestre);
+    } else {
+        echo json_encode($connectionDB);
     }
-
-    $semestre->success = true;
-
-    echo json_encode($semestre);
-} else {
-    echo json_encode($connectionDB);
 }
