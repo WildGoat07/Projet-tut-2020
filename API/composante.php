f0b197eedbd50978dc36899b582a26d640a52c0a
Valentin AZANCOTH <62429527+Valaaz@users.noreply.github.com>
2020-12-09 17:13:23 +0100

    Update composante.php

--- API/composante.php
+++ API/composante.php
@@ -6,7 +6,79 @@ header('Content-Type: application/json');
 $db = new Database();
 
 if ($db) {
-    $requete = $db->query("SELECT * FROM `composante`");
+    $strReq = "SELECT `id_comp`, `nom_comp`, `lieu_comp` FROM `composante`";
+    $postObj = json_decode(file_get_contents('php://input'));
+    if (isset($postObj->filters)) {
+        $firstFilter = true;
+        $whereSet = false;
+        if (isset($postObj->filters->id_comp)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->id_comp as $id_comp) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`id_comp` = \"$id_comp\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->nom_comp)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->nom_comp as $nom_comp) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`nom_comp` = \"$nom_comp\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+        if (isset($postObj->filters->lieu_comp)) {
+            if (!$firstFilter)
+                $strReq .= " AND ";
+            $firstFilter = false;
+            $firstArrayFilter = true;
+            if (!$whereSet) {
+                $strReq .= " WHERE ";
+                $whereSet = true;
+            }
+            $strReq .= '(';
+            foreach ($postObj->filters->lieu_comp as $lieu_comp) {
+                if (!$firstArrayFilter)
+                    $strReq .= " OR ";
+                $strReq .= "`lieu_comp` = \"$lieu_comp\"";
+                $firstArrayFilter = false;
+            }
+            $strReq .= ')';
+        }
+    }
+
+    if (isset($postObj->order))
+        if (isset($postObj->reverse_order) && $postObj->reverse_order)
+            $strReq .= " ORDER BY DESC `$postObj->order`";
+        else
+            $strReq .= " ORDER BY `$postObj->order`";
+    else if (isset($postObj->reverse_order) && $postObj->reverse_order)
+        $strReq .= " ORDER BY DESC `id_comp`";
+    else
+        $strReq .= " ORDER BY `id_comp`";
+    $strReq .= "LIMIT $postObj->quantity OFFSET $postObj->skip";
+
+    $requete = $db->query($strReq);
 
     $composante = new stdClass();
     $composante->values = [];
