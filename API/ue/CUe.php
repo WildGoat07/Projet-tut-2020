6be99627b86ce5790702f18df289e8501089ce7f
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2020-12-10 11:47:39 +0100

    Update CUe.php

--- API/ue/CUe.php
+++ API/ue/CUe.php
@@ -3,13 +3,12 @@ require_once '../app/Database.php';
 
 header('Content-Type: application/json');
 
-$db = new Database();
+$postObj = json_decode(utf8_encode(file_get_contents('php://input')));
 
-if ($db) {
-    
-    $postObj = json_decode(utf8_encode(file_get_contents('php://input')));
+$id_entered=[];
+$indexId=0;
 
-    $presence = new stdClass();
+$presence = new stdClass();
     $presence->code_ue = false;
     $presence->libelle_ue = false;
     $presence->nature = false;
@@ -17,67 +16,75 @@ if ($db) {
     $presence->code_ue_pere = false;
     $presence->code_sem = false;
 
-    $returnedValues = new stdClass;
+$returnedValues = new stdClass;
     $returnedValues->values = [];
-    $returnedValues->succes=false;
+    $returnedValues->success=false;
+    $returnedValues->errors=[];
 
-    foreach ($postObj->values as $values) {
-        $strReq = "INSERT INTO `ue` (";
-        $data = "(";
 
-        $strReq .= " `code_ue` ";
-        $data .= "'$values->code_ue'";
+foreach ($postObj->values as $values) {
+    $strReq = "INSERT INTO `ue` (";
+    $data = "(";
 
-        $strReq .= " ,`libelle_ue` ";
-        $data .= ",'$values->libelle_ue'";
+    $strReq .= " `code_ue` ";
+    $data .= "'$values->code_ue'";
+    array_push($id_entered, $values->code_ue);
 
-        
-        if (isset($values->nature)) {
-            $strReq .= " ,`nature` ";
-            $data .= ",'$values->nature'";
-        }
+    $strReq .= " ,`libelle_ue` ";
+    $data .= ",'$values->libelle_ue'";
 
-        if (isset($values->ECTS)) {
-            $strReq .= " ,`ECTS` ";
-            $data .= ",'$values->ECTS'";
-        }
-
-        if (isset($values->code_ue_pere)) {
-            $strReq .= " ,`code_ue_pere` ";
-            $data .= ",'$values->code_ue_pere'";
-        }
-
-        if (isset($values->code_sem)) {
-            $strReq .= " ,`code_sem`";
-            $data .= ",'$values->code_sem'";
-        }
-
-        $strReq .= ") VALUES $data )";
     
-        $result = $db->query($strReq);
-        
-        if($result) {
-            //on retourne le denrier résultat enregistré
-            $getLast = $db->query("SELECT `code_ue`,`libelle_ue`,`nature`,`ECTS`,`code_ue_pere`,`code_sem` FROM `ue`
-            ORDER BY code_ue DESC
-            LIMIT 1; ");
-
-            $result = $getLast->fetch(PDO::FETCH_OBJ);
-
-            $obj = new stdClass();
-            $obj->code_ue = $result->code_ue;
-            $obj->libelle_ue = $result->libelle_ue;
-            $obj->nature = $result->nature;
-            $obj->ECTS = $result->ECTS;
-            $obj->code_ue_pere = $result->code_ue_pere;
-            $obj->code_sem = $result->code_sem;
-
-            $returnedValues->values[] = $obj;
-            $returnedValues->succes=true;
-        }
+    if (isset($values->nature)) {
+        $strReq .= " ,`nature` ";
+        $data .= ",'$values->nature'";
     }
 
-    echo json_encode($returnedValues);
-} else {
-    echo json_encode($connectionDB);
+    if (isset($values->ECTS)) {
+        $strReq .= " ,`ECTS` ";
+        $data .= ",'$values->ECTS'";
+    }
+
+    if (isset($values->code_ue_pere)) {
+        $strReq .= " ,`code_ue_pere` ";
+        $data .= ",'$values->code_ue_pere'";
+    }
+
+    if (isset($values->code_sem)) {
+        $strReq .= " ,`code_sem`";
+        $data .= ",'$values->code_sem'";
+    }
+
+    $strReq .= ") VALUES $data )";
+
+    $requete=$pdo->prepare($strReq);
+    
+    if ( $requete->execute() ) {
+        $resultStr = "SELECT `code_ue`, `libelle_ue`, `nature`, `ECTS`, `code_ue_pere`, `code_sem` FROM `ue` WHERE ";
+        $resultStr .= "code_ue = $id_entered[$indexId]";
+        $indexId++;
+
+        $result=$pdo->query($resultStr);
+        $row=$result->fetch(PDO::FETCH_OBJ);
+
+        $obj = new stdClass();
+        $obj->code_ue = $row->code_ue;
+        $obj->libelle_ue = $row->libelle_ue;
+        $obj->nature = $row->nature;
+        $obj->ECTS = $row->ECTS;
+        $obj->code_ue_pere = $row->code_ue_pere;
+        $obj->code_sem = $row->code_sem;
+
+        array_push($returnedValues->values, $obj);
+        $returnedValues->success=true;
+    }
+    else {
+        $error=$requete->errorInfo();
+
+        $obj = new stdClass();
+        $obj->error_code = $error[0]; //enregistrement code d'erreur
+        $obj->error_desc = $error[2]; //enregistrement message d'erreru renvoyé
+        array_push($returnedValues->errors, $obj);
+    }
 }
+
+echo json_encode($returnedValues);
