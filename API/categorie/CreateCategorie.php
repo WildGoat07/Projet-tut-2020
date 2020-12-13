4d41224e1500db63e2857d47c56a84731bd1c531
Claire0711 <71119574+Claire0711@users.noreply.github.com>
2020-12-13 16:45:42 +0100

    Can edit + delete a categorie

---
+++ API/categorie/CreateCategorie.php
@@ -0,0 +1,58 @@
+<?php
+require_once '../app/Database.php';
+
+header('Content-Type: application/json');
+
+$postObj = json_decode(utf8_encode(file_get_contents('php://input')));
+
+$id_entered=[];
+$indexId=0;
+
+$returnedValues = new stdClass;
+    $returnedValues->values = [];
+    $returnedValues->success=false;
+    $returnedValues->errors=[];
+
+
+foreach ($postObj->values as $values) {
+    $strReq = "INSERT INTO `categories` (";
+    $data = "(";
+    
+    $strReq .= " `no_cat` ";
+    /*$data .= "'$values->no_cat'";*/
+    array_push($id_entered, $values->no_cat);
+
+    $strReq .= " ,`categorie` ";
+    $data .= ",'$values->categorie'";
+    //array_push($id_entered, $values->categorie);
+    
+
+    $strReq .= ") VALUES $data )";
+
+    $requete=$db->prepare($strReq);
+    
+    if ( $requete->execute() ) {
+        $resultStr = "SELECT `no_cat, categorie` FROM `categories` WHERE ";
+        $resultStr .= "no_cat = $id_entered[$indexId]";
+        $indexId++;
+
+        $result=$db->query($resultStr);
+        $row=$result->fetch(PDO::FETCH_OBJ);
+
+        $obj = new stdClass();
+        $obj->categorie = $row->categorie;
+
+        array_push($returnedValues->values, $obj);
+        $returnedValues->success=true;
+    }
+    else {
+        $error=$requete->errorInfo();
+
+        $obj = new stdClass();
+        $obj->error_code = $error[0]; //enregistrement code d'erreur
+        $obj->error_desc = $error[2]; //enregistrement message d'erreur renvoyé
+        array_push($returnedValues->errors, $obj);
+    }
+}
+
+echo json_encode($returnedValues);
---
+++ API/categorie/EditCategorie.php
@@ -0,0 +1,62 @@
+<?php
+require_once '../app/Database.php';
+
+header('Content-Type: application/json');
+
+$postObj = json_decode(utf8_encode(file_get_contents('php://input')));
+
+$returnedValues = new stdClass;
+    $returnedValues->values = [];
+    $returnedValues->success=false;
+    $returnedValues->errors=[];
+
+
+foreach ($postObj->values as $values) {
+    $strReq = "UPDATE `categories` SET ";
+
+    $data = $values->data;
+
+    if( isset($data->no_cat) ) 
+        $strReq .= "`no_cat` = '$data->no_cat'";
+    if( isset($data->categorie) ) 
+        $strReq .= "`categorie` = '$data->categorie'";
+
+    $target = $values->target;
+    $strReq .= " WHERE `no_cat` = '$target->no_cat' ";
+    
+
+
+    $updateReq=$db->prepare($strReq);
+    if ( $updateReq->execute() ) {
+        $nbRows=$updateReq->rowCount();
+        if( $nbRows != 0) {
+            $resultStr = "SELECT `no_cat, categorie` FROM `categories` WHERE ";
+            $resultStr .= "`no_cat` = '$data->no_cat'";
+
+            $result=$db->query($resultStr);
+            $row=$result->fetch(PDO::FETCH_OBJ);
+
+            $obj = new stdClass();
+            $obj->no_cat = $row->no_cat;
+            $obj->categorie = $row->categorie;
+
+            $returnedValues->values[] = $obj;
+            $returnedValues->success=true;
+        }
+        else {
+            $obj = new stdClass();
+            $obj->error_desc = "0 row affected";
+            $returnedValues->errors[] = $obj;
+        }
+    }
+    else {
+        $error=$updateReq->errorInfo();
+
+        $obj = new stdClass();
+        $obj->error_code = $error[0];
+        $obj->error_desc = $error[2];
+        $returnedValues->errors[] = $obj;
+    }
+}
+
+echo json_encode($returnedValues);
