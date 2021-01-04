f865d4ddb4b05ff30cfc1a33d80a4a6d77efb468
Waynocs <64638869+Waynocs@users.noreply.github.com>
2021-01-04 15:59:42 +0100

    started APICategDAO because bad Interface :)

--- App client/DAO/API/APICategorieDAO.cs
+++ App client/DAO/API/APICategorieDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -16,19 +17,80 @@ namespace DAO.API
 
         private HttpClient Client { get; }
 
-        public Task<Categorie[]> CreateAsync(IEnumerable<Categorie> values)
+        public async Task<Categorie[]> CreateAsync(IEnumerable<Categorie> values)
         {
-            throw new NotImplementedException();
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+            var obj = new
+            {
+                values = values.ToArray()
+            };
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("categorie/CreateCategorie.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<Categorie>>(await response.Content.ReadAsStringAsync());
+            if (status.success)
+                return status.values;
+            else
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, err.error_code switch
+                {
+                    "23000" => DAOException.ErrorCode.EXISTING_ENTRY,
+                    _ => DAOException.ErrorCode.UNKNOWN
+                });
+            }
         }
 
-        public Task DeleteAsync(IEnumerable<Categorie> values)
+        public async Task DeleteAsync(IEnumerable<Categorie> values)
         {
-            throw new NotImplementedException();
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+            var obj = new
+            {
+                values = (from value in values
+                          select new
+                          {
+                              value.no_cat
+                          }).ToArray()
+            };
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("categorie/DeleteCategorie.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<Categorie>>(await response.Content.ReadAsStringAsync());
+            if (!status.success)
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, err.error_code switch
+                {
+                    "66666" => DAOException.ErrorCode.MISSING_ENTRY,
+                    "23000" => DAOException.ErrorCode.ENTRY_LINKED,
+                    _ => DAOException.ErrorCode.UNKNOWN
+                });
+            }
         }
 
-        public Task<Categorie[]> GetByIdAsync(IEnumerable<int> id)
+        public async Task<Categorie[]> GetByIdAsync(IEnumerable<int> no)
         {
-            throw new NotImplementedException();
+            if (no == null)
+                throw new ArgumentNullException(nameof(no));
+            var obj = new Dictionary<string, object>();
+            var filters = new Dictionary<string, object>();
+            obj.Add("filters", filters);
+            obj.Add("quantity", no.Count());
+            obj.Add("skip", 0);
+            filters.Add("no_cat", no.ToArray());
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("categorie/SelectCategorie.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<Categorie>>(await response.Content.ReadAsStringAsync());
+            if (status.success)
+                return status.values;
+            else
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, DAOException.ErrorCode.UNKNOWN);
+            }
         }
 
         public Task<Categorie[]> GetFilteredAsync(int maxCount, int page, string? orderBy = null, bool reverseOrder = false, string? search = null)
