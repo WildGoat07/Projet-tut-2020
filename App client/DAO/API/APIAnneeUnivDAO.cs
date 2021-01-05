78a982c4c4f1962be4951669b960bbbff2ce4d29
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2021-01-05 12:13:30 +0100

    Update APIAnneeUnivDAO.cs

--- App client/DAO/API/APIAnneeUnivDAO.cs
+++ App client/DAO/API/APIAnneeUnivDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace DAO.API
 {
@@ -16,24 +17,109 @@ namespace DAO.API
 
         private HttpClient Client { get; }
 
-        public Task<AnneeUniv[]> CreateAsync(IEnumerable<AnneeUniv> values)
+        public async Task<AnneeUniv[]> CreateAsync(IEnumerable<AnneeUniv> values)
         {
-            throw new NotImplementedException();
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+            var obj = new
+            {
+                values = values.ToArray()
+            };
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("annee_univ/CreateAnneeUniv.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<AnneeUniv>>(await response.Content.ReadAsStringAsync());
+            if (status.success)
+                return status.values;
+            else
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, err.error_code switch
+                {
+                    "23000" => DAOException.ErrorCode.EXISTING_ENTRY,
+                    _ => DAOException.ErrorCode.UNKNOWN
+                });
+            }
         }
 
-        public Task DeleteAsync(IEnumerable<AnneeUniv> values)
+        public async Task DeleteAsync(IEnumerable<AnneeUniv> values)
         {
-            throw new NotImplementedException();
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+            var obj = new
+            {
+                values = (from value in values
+                          select new
+                          {
+                              value.annee
+                          }).ToArray()
+            };
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("annee_univ/DeleteAnneeUniv.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<AnneeUniv>>(await response.Content.ReadAsStringAsync());
+            if (!status.success)
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, err.error_code switch
+                {
+                    "66666" => DAOException.ErrorCode.MISSING_ENTRY,
+                    "23000" => DAOException.ErrorCode.ENTRY_LINKED,
+                    _ => DAOException.ErrorCode.UNKNOWN
+                });
+            }
         }
 
-        public Task<AnneeUniv[]> GetAllAsync()
+        public async Task<AnneeUniv[]> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var obj = new Dictionary<string, object>();
+            obj.Add("quantity", 100);
+            obj.Add("skip", 0);
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("annee_univ/SelectAnneeUniv.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<AnneeUniv>>(await response.Content.ReadAsStringAsync());
+            if (status.success)
+                return status.values;
+            else
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, DAOException.ErrorCode.UNKNOWN);
+            }
         }
 
-        public Task<AnneeUniv[]> UpdateAsync(IEnumerable<(AnneeUniv, AnneeUniv)> values)
+        public async Task<AnneeUniv[]> UpdateAsync(IEnumerable<(AnneeUniv, AnneeUniv)> values)
         {
-            throw new NotImplementedException();
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+            var obj = new
+            {
+                values = (from value in values
+                          select new
+                          {
+                              target = new
+                              {
+                                  value.Item1.annee
+                              },
+                              data = value.Item2
+                          }).ToArray()
+            };
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("annee_univ/EditAnneeUniv.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<AnneeUniv>>(await response.Content.ReadAsStringAsync());
+            if (status.success)
+                return (from value in values select value.Item2).ToArray();
+            else
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, err.error_code switch
+                {
+                    "66666" => DAOException.ErrorCode.MISSING_ENTRY,
+                    "23000" => DAOException.ErrorCode.ENTRY_LINKED,
+                    _ => DAOException.ErrorCode.UNKNOWN
+                });
+            }
         }
     }
 }
