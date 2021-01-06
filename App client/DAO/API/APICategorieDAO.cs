1789ce184a6c63ae62aa6432e9e627b821ac93e3
Waynocs <64638869+Waynocs@users.noreply.github.com>
2021-01-06 14:33:33 +0100

    finished APICategorieAPI and corrected APISemestreDAO

--- App client/DAO/API/APICategorieDAO.cs
+++ App client/DAO/API/APICategorieDAO.cs
@@ -85,7 +85,7 @@ namespace DAO.API
             var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
             var status = JsonConvert.DeserializeObject<Response<Categorie>>(await response.Content.ReadAsStringAsync());
             if (status.success)
-                return status.values.Length == id.Count() ? status.values : throw new DAOException("An entry is missing", DAOException.ErrorCode.MISSING_ENTRY);
+                return status.values.Length == no.Count() ? status.values : throw new DAOException("An entry is missing", DAOException.ErrorCode.MISSING_ENTRY);
             else
             {
                 var err = status.errors.First();
@@ -93,14 +93,64 @@ namespace DAO.API
             }
         }
 
-        public Task<Categorie[]> GetFilteredAsync(int maxCount, int page, string? orderBy = null, bool reverseOrder = false, string? search = null)
+        public async Task<Categorie[]> GetFilteredAsync(int maxCount, int page, string? orderBy = null, bool reverseOrder = false, string? search = null)
         {
-            throw new NotImplementedException();
+            var obj = new Dictionary<string, object>();
+            var filters = new Dictionary<string, object>();
+            obj.Add("filters", filters);
+            obj.Add("quantity", maxCount);
+            obj.Add("skip", maxCount * page);
+            if (orderBy != null)
+                obj.Add("order", orderBy);
+            obj.Add("reverse_order", reverseOrder);
+            if (search != null)
+                obj.Add("search", search);
+            
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("categorie/SelectCategorie.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<Categorie>>(await response.Content.ReadAsStringAsync());
+            if (status.success)
+                return status.values;
+            else
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, DAOException.ErrorCode.UNKNOWN);
+            }
         }
 
-        public Task<Categorie[]> UpdateAsync(IEnumerable<(Categorie, Categorie)> values)
+        public async Task<Categorie[]> UpdateAsync(IEnumerable<(Categorie, Categorie)> values)
         {
-            throw new NotImplementedException();
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+            var obj = new
+            {
+                values = (from value in values
+                          select new
+                          {
+                              target = new
+                              {
+                                  value.Item1.no_cat
+                              },
+                              data = value.Item2
+                          }).ToArray()
+            };
+            var jsonObj = JsonConvert.SerializeObject(obj, Formatting.None);
+            var url = new Uri("categorie/EditCategorie.php", UriKind.Relative);
+            var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
+            var status = JsonConvert.DeserializeObject<Response<Categorie>>(await response.Content.ReadAsStringAsync());
+            if (status.success)
+                return (from value in values select value.Item2).ToArray();
+            else
+            {
+                var err = status.errors.First();
+                throw new DAOException(err.error_desc, err.error_code switch
+                {
+                    "66666" => DAOException.ErrorCode.MISSING_ENTRY,
+                    "23000" => DAOException.ErrorCode.ENTRY_LINKED,
+                    _ => DAOException.ErrorCode.UNKNOWN
+                });
+            }
         }
     }
 }
--- App client/DAO/API/APISemestreDAO.cs
+++ App client/DAO/API/APISemestreDAO.cs
@@ -85,7 +85,7 @@ namespace DAO.API
             var response = await Client.PostAsync(url, new StringContent(jsonObj, Encoding.UTF8, "application/json"));
             var status = JsonConvert.DeserializeObject<Response<Semestre>>(await response.Content.ReadAsStringAsync());
             if (status.success)
-                return status.values.Length == id.Count() ? status.values : throw new DAOException("An entry is missing", DAOException.ErrorCode.MISSING_ENTRY);
+                return status.values.Length == code.Count() ? status.values : throw new DAOException("An entry is missing", DAOException.ErrorCode.MISSING_ENTRY);
             else
             {
                 var err = status.errors.First();
