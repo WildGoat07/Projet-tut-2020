4924500f56010180bc4c3e78d42d03ac1751c257
Valaaz <val.zancoth@gmail.com>
2021-01-13 23:28:17 +0100

    added module ec (incomplete)
    need to do validate() and validation_click()

---
+++ App client/GUI/modules/EditEcModule.cs
@@ -0,0 +1,30 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI.modules
+{
+    public class EditEcModule : Module
+    {
+        private readonly bool create;
+
+        public EditEcModule(Ec? ec)
+        {
+            Content = new UI.EditEc(ec, this);
+            Title = ec == null ? "Créer un ec" : $"Édition de {ec.libelle_ec}";
+            create = ec != null;
+        }
+
+        public override bool Closeable => true;
+
+        public override UI.EditEc Content { get; }
+
+        public override string Title { get; }
+
+        public override async Task RefreshAsync() => await Content.RefreshAsync();
+    }
+}
