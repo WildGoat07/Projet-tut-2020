557c4c692035b7977c41a57a70fe3599dc3297ce
Valaaz <val.zancoth@gmail.com>
2021-01-14 14:04:36 +0100

    added module parametrages

---
+++ App client/GUI/modules/ParametrageModule.cs
@@ -0,0 +1,27 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI.modules
+{
+    public class ParametrageModule : Module
+    {
+        public ParametrageModule()
+        {
+            Content = new UI.Parametrage(this);
+            Title = "Parametrage";
+        }
+
+        public override bool Closeable => true;
+
+        public override UI.Parametrage Content { get; }
+
+        public override string Title { get; }
+
+        public override async Task RefreshAsync() => await Content.RefreshAsync();
+    }
+}
